bb48a4464dd171e8b670d22f7356f2b5e491e781
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-09-27 11:36:29 +0300

    some of data apply it in card

--- App.js
+++ App.js
@@ -6,9 +6,29 @@ Date = [
     { name: "جكن اكسبريس", location: "المنصور - شارع 14 رمضان", rate: 2.8 },
 ];
 
-console.log("the array is:", Date);
 
-Date.map(function (item) {
-    console.log(item);
-    document.write(item.name, item.location, item.rate);
-});
+const cardContainer = document.getElementsByClassName('container')[0];
+
+
+function createCard() {      
+    Date.map(function (item) {
+        const elementData = document.createElement('div');
+        elementData.classList.add('card');
+        elementData.innerHTML = `
+                <div class="card-image">
+                    <img src="https://cdn.pixabay.com/photo/2023/11/22/21/11/ai-generated-8406464_1280.png" alt="">
+                </div>
+                <div class="card-text">
+                    <h2 class="card-title">${item.name}</h2>
+                    <p class="card-body">${item.location}</p>
+                </div>
+                <div class="card-rate">${item.rate}
+                    <i class="fa fa-star-o" style="font-size:20px;color:rgb(255, 238, 0);padding:5px;"></i>
+                </div>
+        `
+        cardContainer.appendChild(elementData);
+    })
+}
+createCard();
+
+
